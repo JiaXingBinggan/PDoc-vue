e715bb9fd2212cfca851fc515a9b5dd3d83a641b
JiaXingBingGan <729869585@qq.com>
2017-06-19 17:28:10 +0800

    新增用户个人信息管理

--- src/components/user/userinfo.vue
+++ src/components/user/userinfo.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="user-info">{{msg}}</div>
+  <div class="user-info">
+  	<mu-row>
+	    <mu-col width="100" tablet="50" desktop="20">
+	    	<div class="info-menu">
+	    		<mu-paper class="demo-menu">
+				    <mu-menu>
+				      <mu-menu-item title="个人信息"/>
+				      <mu-menu-item title="账户修改"/>
+				      <mu-menu-item title="修改密码"/>
+				    </mu-menu>
+				  </mu-paper>
+	    	</div>
+	    </mu-col>
+	    <mu-col width="100" tablet="50" desktop="80">
+	    	<div class="info-block"></div>
+	    </mu-col>
+	  </mu-row>
+  </div>
 </template>
 
 <script>
@@ -15,9 +32,20 @@ export default {
 
 <style lang="stylus" scoped>
 .user-info
-  width 800px
-  height 400px
+  width 1000px
+  height 600px
   margin 0px auto 
   margin-top 20px
-  background orange
+.user-info div[class*="col-"] 
+  background #fff
+  text-align center
+  color #000
+.info-menu 
+  width 100%
+  height 500px
+	.demo-menu 
+	  display inline-block
+.info-block
+  width 100%
+  height 500px
 </style>
